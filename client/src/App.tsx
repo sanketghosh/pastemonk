fd28f610be58589dbc9ab058e687f981d2519069
sanket <code.sanket13@gmail.com>
2024-01-22 00:57:05 +0530

    refactor: frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import Editor from "./components/editor";
 
 export default function App() {
   return (
-    <main className="h-screen border-none bg-zinc-900 p-3 md:p-5 lg:p-6">
-      <Toaster />
+    <main className="h-screen border-none bg-zinc-900 p-3 md:p-5 lg:p-6 xl:p-8">
+      <Toaster position="top-right" />
       <Editor />
     </main>
   );
--- client/src/components/editor.tsx
+++ client/src/components/editor.tsx
@@ -1,7 +1,9 @@
 /** components */
 import { Button } from "./ui";
 import { CopyIcon } from "../assets";
+import GithubIcon from "../assets/github-icon";
 
+/** packages */
 import {
   BoldItalicUnderlineToggles,
   MDXEditor,
@@ -15,8 +17,6 @@ import toast from "react-hot-toast";
 
 export default function Editor() {
   const [editorText, setEditorText] = useState<string>("");
-
-  console.log(editorText);
 
   const copyText = async () => {
     try {
@@ -33,21 +33,43 @@ export default function Editor() {
     <MDXEditor
       markdown={editorText}
       onChange={(text) => setEditorText(text)}
+      placeholder="Write whatever you want."
       plugins={[
         headingsPlugin(),
         toolbarPlugin({
-          toolbarContents: () => (
-            <div className="flex items-center gap-3">
-              <UndoRedo />
-              <BoldItalicUnderlineToggles />
-              <Button copyText={copyText}>
-                <CopyIcon size={18.5} />
-              </Button>
-            </div>
-          ),
+          toolbarContents: () => <ToolBarFunctions copyText={copyText} />,
         }),
       ]}
-      className="dark-theme dark-editor h-full w-full overflow-y-scroll rounded-lg bg-zinc-950"
+      className="dark-theme dark-editor h-full w-full overflow-y-scroll rounded-xl bg-black"
     />
+  );
+}
+
+/** toolbar function */
+
+interface ToolBarFunctionsProps {
+  copyText: () => void;
+}
+
+export function ToolBarFunctions({ copyText }: ToolBarFunctionsProps) {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div className="flex items-center gap-3">
+        <UndoRedo />
+        <BoldItalicUnderlineToggles />
+        <Button copyText={copyText}>
+          <CopyIcon size={18.5} />
+        </Button>
+      </div>
+      <div className="flex items-center px-3">
+        <a
+          href="https://github.com/sanketghosh/pastemonk"
+          target="_blank"
+          className="rounded-sm p-1.5 transition-all hover:bg-neutral-700"
+        >
+          <GithubIcon size={19} />
+        </a>
+      </div>
+    </div>
   );
 }
